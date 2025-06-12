afb586aa547f89e6e86773cdce9f6241795117d8
hamzathul dilshad <diluhd99@gmail.com>
2025-06-12 20:27:45 +0530

    feat(event-chat): add initial EventChatComponent with template and styles

---
+++ frontend/src/app/modules/user/chat/event-chat/event-chat.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-chat',
+  imports: [],
+  templateUrl: './event-chat.component.html',
+  styleUrl: './event-chat.component.css'
+})
+export class EventChatComponent {
+
+}
