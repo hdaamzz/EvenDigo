b39b6bde5c1014da5fe4f9273cb9be9aa20b1205
hamzathul dilshad <diluhd99@gmail.com>
2025-07-03 09:41:35 +0530

    feat: add environment configuration file for frontend

---
+++ frontend/src/app/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment={
+    apiUrl:process.env['NG_APP_API_URL'] || `http://localhost:3000/api/`,
+    baseUrl:process.env['NG_APP_BASE_URL'] ||'http://localhost:3000',
+    production:  process.env['NG_APP_PRODUCTION'] === 'true' || false,
+    stripePublishKey:process.env['NG_APP_STRIPE_KEY'] || '',
+    zegoAppId: parseInt(process.env['NG_APP_ZEGO_APP_ID'] ||''),
+    firebase: {
+        apiKey: process.env['NG_APP_FIREBASE_API_KEY'] ||"",
+        authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'] ||  "",
+        projectId:process.env['NG_APP_FIREBASE_PROJECT_ID'] || "",
+        storageBucket:  process.env['NG_APP_FIREBASE_STORAGE_BUCKET'] || "",
+        messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'] || "",
+        appId:process.env['NG_APP_FIREBASE_APP_ID'] || ""
+    }
+}
+
